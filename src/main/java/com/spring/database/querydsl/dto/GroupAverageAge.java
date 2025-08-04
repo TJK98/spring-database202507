93e7f8c522ec7abb496b95b43388098051838242
TJK98 <wtj1998@naver.com>
2025-08-04 11:04:12 +0900

    QueryDSL 학습 추가: 동적 쿼리 및 서브쿼리 테스트 코드 구현, GroupRepositoryCustom 인터페이스 및 구현체 작성, 그룹별 평균 나이 조회 API 추가, DTO 필드명 변경 및 JSON 매핑 설정

--- src/main/java/com/spring/database/querydsl/dto/GroupAverageAge.java
+++ src/main/java/com/spring/database/querydsl/dto/GroupAverageAge.java
@@ -1,5 +1,6 @@
 package com.spring.database.querydsl.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.querydsl.core.Tuple;
 import com.spring.database.querydsl.entity.QIdol;
 import lombok.*;
@@ -15,8 +16,10 @@ import lombok.*;
 // 그룹명과 평균 나이를 매핑할 클래스
 public class GroupAverageAge {
 
+    @JsonProperty("name")
     private String groupName;
-    private double averageAge;
+    @JsonProperty("age")
+    private Double averageAge;
 
     // Tuple을 전달 받아서 DTO로 변환하는 생성자
     public GroupAverageAge(Tuple tuple) {
---
+++ src/main/java/com/spring/database/querydsl/service/IdolService.java
@@ -0,0 +1,24 @@
+package com.spring.database.querydsl.service;
+
+import com.spring.database.querydsl.dto.GroupAverageAge;
+import com.spring.database.querydsl.entity.Group;
+import com.spring.database.querydsl.repository.GroupRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class IdolService {
+
+    private final GroupRepository groupRepository;
+
+    // 그룹별 평균 나이 조회
+    public List<GroupAverageAge> average() {
+        List<GroupAverageAge> averageAgeList = groupRepository.groupAverage();
+        return averageAgeList;
+    }
+}
